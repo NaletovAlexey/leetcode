001299db8aa58c551aa20443e26ec1f9067bbc8b
Alexey Naletov <alexey.naletov@tewris.com>
2026-06-03 00:16:37 +0300

    [System design]: Q1. LRU Cache

---
+++ src/main/java/com/program/training/systemdesign/cache/LRUCache.java
@@ -0,0 +1,60 @@
+package com.program.training.systemdesign.cache;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+ *
+ * @author naletov
+ */
+public class LRUCache
+{
+    private final Map<Integer,Integer> cache;
+    /**
+     * LRUCache(int capacity) Initialize the LRU cache with positive size capacity
+     */
+    public LRUCache(int capacity)
+    {
+        this.cache = new LinkedHashMap<>(capacity, 0.75f, true)
+        {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest)
+            {
+                return size() > capacity;
+            }
+        };
+    }
+
+    /**
+     * Return the value of the key if the key exists, otherwise return -1.
+     * @param key Cache key
+     * @return Cache value
+     */
+    public int get(int key)
+    {
+        if (cache.containsKey(key))
+        {
+            return cache.get(key);
+        }
+        return -1;
+    }
+
+    /**
+     * Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache.
+     * If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+     * @param key Cache key
+     * @param value Cache value
+     */
+    public void put(int key, int value)
+    {
+        cache.put(key, value);
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
---
+++ src/test/java/com/program/training/systemdesign/cache/LRUCacheTest.java
@@ -0,0 +1,35 @@
+package com.program.training.systemdesign.cache;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Q1. LRU Cache
+ * @author naletov
+ */
+class LRUCacheTest
+{
+    LRUCache lRUCache;
+
+    @BeforeEach
+    void setup()
+    {
+        lRUCache = new LRUCache(2);
+    }
+
+    @Test
+    void testLRUCache()
+    {
+        lRUCache.put(1, 1);                             // cache is {1=1}
+        lRUCache.put(2, 2);                             // cache is {1=1, 2=2}
+        assertEquals(1, lRUCache.get(1));      // return 1
+        lRUCache.put(3, 3);                             // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+        assertEquals(-1, lRUCache.get(2));     // returns -1 (not found)
+        lRUCache.put(4, 4);                             // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+        assertEquals(-1, lRUCache.get(1));     // return -1 (not found)
+        assertEquals(3, lRUCache.get(3));      // return 3
+        assertEquals(4, lRUCache.get(4));      // return 4
+    }
+}
